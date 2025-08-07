41611d728f8b66e63ddc98b283ab3bac395eec40
YoussefAhmed108 <youssef.ahmed108@gmail.com>
2025-08-07 17:38:28 +0300

    Chapter 17 done

--- clox/main.c
+++ clox/main.c
@@ -2,11 +2,13 @@
 #include "chunk.h"
 #include "debug.h"
 #include "memory.h"
+#include "vm.h"
 
 #include <stdio.h>
 
 
 int main(int argc, const char* argv[]) {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
@@ -14,6 +16,8 @@ int main(int argc, const char* argv[]) {
     writeChunk(&chunk, constant , 123);
     writeChunk(&chunk, OP_RETURN , 123);
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
